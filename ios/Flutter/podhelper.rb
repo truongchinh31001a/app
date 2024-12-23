c222f9b28a38a20be2c363e8db9e32f3364da1cf
truongchinh31001a <truongchinh31001@gmail.com>
2024-12-23 09:29:05 +0700

    Fix missing podhelper.rb

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,2 @@
+# flutter_install_all_ios_pods(File.dirname(File.realpath(__FILE__)))
+require File.expand_path(File.join('packages', 'flutter_tools', 'bin', 'podhelper.rb'), File.dirname(File.dirname(File.realpath(__FILE__))))
